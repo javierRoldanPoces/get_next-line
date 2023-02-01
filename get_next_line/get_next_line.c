8574c9ef75a3a25b379f203e0170249c4690acae
Javier Roldan poces <javierroldanpoces@gmail.com>
2023-02-01 19:54:25 +0100

    actualizada cabecera get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
